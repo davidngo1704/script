1067f8834607985d5d6843762c1f41d6d4318f33
davidngo1704 <davidngo1704@gmail.com>
2025-12-09 22:00:55 +0700

    auto commit at 2025-12-09 22:00:55

---
+++ script.sh
@@ -0,0 +1,3 @@
+#curl -sL https://raw.githubusercontent.com/davidngo1704/script/master/test.sh | bash
+
+curl -H "Cache-Control: no-cache" -s https://raw.githubusercontent.com/davidngo1704/script/master/test.sh
